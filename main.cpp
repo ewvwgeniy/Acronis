ad8a8d99e5512f6e63188109a1cf4b94de6924d7
ewvwgeniy <65627320+ewvwgeniy@users.noreply.github.com>
2020-05-20 23:03:41 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,11 +9,11 @@
 #include <unordered_map>
 #include <string>
 
-#define N 6 // size of substring
+#define N 5 // size of substring
 
 using namespace std;
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]) // 1- name of directory with files  2 - name of file to search in
 {
     unordered_map <string, int> mp;
     int fd, n, i;
@@ -45,9 +45,8 @@ int main(int argc, char *argv[])
                             else {
 
                                 if (statbuf.st_size >= N) {
-                                    for (i = 0; i < statbuf.st_size - 1; i++) {
+                                    for (i = 0; i < statbuf.st_size - N + 1; i++) {
                                         snprintf(buf, N + 1, "%s", mmf + i);
-                                        cout << buf << endl;
                                         string sub_str = buf;
                                         if(mp.find(sub_str) != mp.end())
                                             mp[sub_str] = 2;
@@ -72,6 +71,20 @@ int main(int argc, char *argv[])
 			if (closedir(dir) == -1)
                 perror("closedir");
         }
+    }
+
+    // poisk podstrok iz faila
+
+    int file_fd;
+
+
+    if ((file_fd = open(argv[2], O_RDONLY)) == -1)
+        perror("open");
+    else {
+
+
+        if (close(file_fd) == -1)
+            perror("close");
     }
     return 0;
 }
